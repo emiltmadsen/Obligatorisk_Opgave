8dcd1b4d0b652bb4b8884c460336c6eaba8ceb1e
Emil <emil078a@stud.kea.dk>
2020-10-15 14:47:02 +0200

    populated classes and files and seeded sql for the h2 database

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,19 @@
 package com.example.demo;
 
+import com.example.demo.repository.EmployeeRepository;
+import com.example.demo.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+
+    @Autowired
+    EmployeeRepository empRepo;
+
+    @Autowired
+    ProjectRepository proRepo;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,7 +1,33 @@
 package com.example.demo.controllers;
 
+import com.example.demo.entities.Employee;
+import com.example.demo.entities.Project;
+import com.example.demo.repository.EmployeeRepository;
+import com.example.demo.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    ProjectRepository proRepo;
+
+    @Autowired
+    EmployeeRepository empRepo;
+
+    @GetMapping("/")
+    public String displayHome(Model model) {
+        List<Project> projects = proRepo.findAll();
+        model.addAttribute("projectsList", projects);
+
+        List<Employee> employees = empRepo.findAll();
+        model.addAttribute("employeesList", employees);
+
+        return "home";
+    }
 }
